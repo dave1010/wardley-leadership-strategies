1f5ee2abecc1e0d7ed37e56cce020fc1e3b8c4a2
Dave Hulbert <dave1010@gmail.com>
2025-03-15 23:31:38 +0000

    Title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,8 +39,8 @@ export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="The Compendium of Wardley Mapping Leadership Gameplays and Strategies">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
