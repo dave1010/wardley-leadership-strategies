ef29ca79c0fe505e79fc116af5cd6e61bf747b2a
Dave Hulbert <dave1010@gmail.com>
2026-01-18 15:51:46 +0000

    Update markdown hook and node engine

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -35,7 +35,6 @@ const config: Config = {
   projectName: 'wardley-leadership-strategies', // Usually your repo name.
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
@@ -306,6 +305,9 @@ const config: Config = {
 
   markdown: {
     mermaid: true,
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
   },
   themes: ['@docusaurus/theme-mermaid'],
 
